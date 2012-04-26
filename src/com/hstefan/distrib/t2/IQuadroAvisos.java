315bef32329c1f664978bcff55a15a4bbf935619
yuriks <yuriks@yuriks.net>
2012-04-26 00:28:49 -0300

    Added javadoc to interfaces.

--- src/com/hstefan/distrib/t2/IQuadroAvisos.java
+++ src/com/hstefan/distrib/t2/IQuadroAvisos.java
@@ -1,5 +1,17 @@
 package com.hstefan.distrib.t2;
 
+/**
+ * A bulletin board client. It can send and receive message to/from a
+ * {@link IServidorAvisos}.
+ *
+ * @author yuriks
+ */
 public interface IQuadroAvisos {
+
+	/**
+	 * Called by {@link IServidorAvisos} when a new message is broadcasted.
+	 *
+	 * @param mensagem The message contents.
+	 */
 	void notificar(String mensagem);
 }
