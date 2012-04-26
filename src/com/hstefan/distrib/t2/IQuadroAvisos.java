a47763d7ca632aa77163cb38c173b5da3cfc1fa6
yuriks <yuriks@yuriks.net>
2012-04-26 00:10:12 -0300

    Created interfaces as defined in the assignment spec.

---
+++ src/com/hstefan/distrib/t2/IQuadroAvisos.java
@@ -0,0 +1,5 @@
+package com.hstefan.distrib.t2;
+
+public interface IQuadroAvisos {
+	void notificar(String mensagem);
+}
---
+++ src/com/hstefan/distrib/t2/IServidorAvisos.java
@@ -0,0 +1,6 @@
+package com.hstefan.distrib.t2;
+
+public interface IServidorAvisos {
+	void setAviso(String mensagem);
+	void setQuadro(IQuadroAvisos quadro);
+}
