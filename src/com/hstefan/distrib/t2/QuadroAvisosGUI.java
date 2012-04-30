43d5b4d2e46a0eb663151caf954a9bf65f1cfeaa
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-04-29 23:47:25 -0300

    Using system's look and feel on the GUI.

--- src/com/hstefan/distrib/t2/QuadroAvisosGUI.java
+++ src/com/hstefan/distrib/t2/QuadroAvisosGUI.java
@@ -18,6 +18,14 @@ public class QuadroAvisosGUI {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+		} catch (InstantiationException e) {
+		} catch (IllegalAccessException e) {
+		} catch (UnsupportedLookAndFeelException e) {
+		}
+		
 		QuadroAvisosGUI window = new QuadroAvisosGUI();
 		QuadroAvisos bboard;
 		try {
