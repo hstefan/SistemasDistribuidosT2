f8600d7719b9512b0094271cf1f6304577d990f1
Hugo Puhlmann <hugopuhlmann@gmail.com>
2012-05-30 21:33:44 -0300

    Implementing the peep-to-peer bulletin board. Every instance of QuadoAvisos will broadcast a message with it's host information, making if possible for the other instances in the group to look up this instance on the RMI registry and call the "notificar" method, which publishes the bulletin on that board.

--- src/com/hstefan/distrib/t2/QuadroAvisos.java
+++ src/com/hstefan/distrib/t2/QuadroAvisos.java
@@ -1,10 +1,14 @@
 package com.hstefan.distrib.t2;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-import java.rmi.server.UnicastRemoteObject;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.rmi.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 
 /**
@@ -13,42 +17,57 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("serial")
 public class QuadroAvisos
-		extends UnicastRemoteObject
-		implements IQuadroAvisos {
+        extends Peer
+        implements IQuadroAvisos, Remote {
+    
+    private QuadroAvisosGUI gui;
+    private Set<HostEntry> mGroupAdresses;
+    
+    public static final String REG_NAME = "QuadroAvisos";
 
-	private QuadroAvisosGUI gui;
-	private IServidorAvisos server;
+    @SuppressWarnings("LeakingThisInConstructor")
+    public QuadroAvisos(QuadroAvisosGUI gui, String host, int port) throws RemoteException {
+        this.gui = gui;
+        mGroupAdresses = new HashSet<HostEntry>();
+        
+        Registry reg = LocateRegistry.createRegistry(port);
+        try {
+            reg.bind(REG_NAME, this);
+        } catch (AlreadyBoundException ex) {
+            //TODO
+        } catch (AccessException ex) {
+            //TODO
+        }
+    }
 
-	@SuppressWarnings("LeakingThisInConstructor")
-	public QuadroAvisos(QuadroAvisosGUI gui, String host, int port) throws RemoteException {
-		this.gui = gui;
-		try {
-			StringBuilder bHost = new StringBuilder();
-			bHost.append("//");
-			bHost.append(host);
-			bHost.append(":");
-			bHost.append(port);
-			bHost.append("/ServidorAvisos");
-			
-			server = (IServidorAvisos) Naming.lookup(bHost.toString());
-			server.setQuadro(this);
-		} catch (NotBoundException ex) {
-			System.out.println("Bulletin board server not found!");
-		} catch (MalformedURLException ex) {
-			throw new IllegalArgumentException("Invalid URL", ex);
-		}
-	}
+    public void notificar(final String mensagem) throws RemoteException {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                gui.receiveMessage(mensagem);
+            }
+        });
+    }
 
-	public void notificar(final String mensagem) throws RemoteException {
-		SwingUtilities.invokeLater(new Runnable() {
+    public void broadcast(String mensagem) throws RemoteException {
+        Registry r;
+        QuadroAvisos qr;
+    
+        for(HostEntry e : mGroupAdresses){
+            r = LocateRegistry.getRegistry(e.adress.toString(), e.port);
+            try {
+                qr = (QuadroAvisos)r.lookup(e.reg_name);
+                qr.notificar(mensagem);
+            } catch (NotBoundException ex) {
+                mGroupAdresses.remove(e);
+            } catch (AccessException ex) {
+                //TODO
+            }
+        }
+    }
 
-			public void run() {
-				gui.receiveMessage(mensagem);
-			}
-		});
-	}
-
-	public void broadcast(String mensagem) throws RemoteException {
-		server.setAviso(mensagem);
-	}
+    @Override
+    public void receiveMessage(DatagramPacket packet) {
+        mGroupAdresses.add(new HostEntry(packet.getAddress(), packet.getPort(), 
+                new String(packet.getData()) ) );
+    }
 }
