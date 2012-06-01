ed632dca3909da5281ab75f132089f8808069b78
yuriks <yuriks@yuriks.net>
2012-05-31 22:12:25 -0300

    Added hashCode and equals methods to HostEntry

--- src/com/hstefan/distrib/t2/HostEntry.java
+++ src/com/hstefan/distrib/t2/HostEntry.java
@@ -20,4 +20,34 @@ class HostEntry {
         this.port = port;
         this.reg_name = reg_name;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final HostEntry other = (HostEntry) obj;
+        if (this.adress != other.adress && (this.adress == null || !this.adress.equals(other.adress))) {
+            return false;
+        }
+        if (this.port != other.port) {
+            return false;
+        }
+        if ((this.reg_name == null) ? (other.reg_name != null) : !this.reg_name.equals(other.reg_name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + (this.adress != null ? this.adress.hashCode() : 0);
+        hash = 67 * hash + this.port;
+        hash = 67 * hash + (this.reg_name != null ? this.reg_name.hashCode() : 0);
+        return hash;
+    }
 }
